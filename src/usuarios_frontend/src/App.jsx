92d0310bf8cf8978bfcf3024d14d0cff920ce5f5
Rony-san <ronysantiagoba@gmail.com>
2024-05-09 13:31:07 -0500

    El front no reconoce tailwind

--- src/usuarios_frontend/src/App.jsx
+++ src/usuarios_frontend/src/App.jsx
@@ -1,21 +1,52 @@
-import  Menu  from './components/Menu';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import Hero from "./components/Hero/Hero";
+import Quotes from "./components/Quotes/Quotes";
+import Banner from "./components/Banner/Banner";
+import Banner2 from "./components/Banner/Banner2";
+import Features from "./components/Features/Features";
+import AppStore from "./components/AppStore/AppStore";
+import Footer from "./components/Footer/Footer";
+import PopupPlayer from "./components/PopupPlayer/PopupPlayer";
+import Menu from "./components/Menu";
 
-import Home from './components/Home';
-import Users from './components/Users';
-import UserCreate from './components/UserCreate';
-import Prueba from './prueba/Prueba';
 
-import {BrowserRouter, Route, Link, Routes} from 'react-router-dom';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
-function App(){
-    return(
-        <div className='contanier' >
-            <Menu />
-        </div>
-            
+const App = () => {
+  const [isPlay, setIsPlay] = useState(false);
 
-    )
-}
+  const togglePlay = () => {
+    setIsPlay(!isPlay);
+  };
 
-export default App
+  useEffect(() => {
+    AOS.init({
+      offset: 100,
+      duration: 800,
+      easing: "ease-in-sine",
+      delay: 100,
+    });
+    AOS.refresh();
+  }, []);
+
+  return (
+    <main className="overflow-x-hidden bg-white dark:bg-black text-black dark:text-white duration-300">
+        {/* <Menu/> */}
+      <Navbar />
+      <Hero togglePlay={togglePlay} />
+      <Quotes />
+      <Banner togglePlay={togglePlay} />
+      <Banner2 togglePlay={togglePlay} />
+      <Features />
+      <AppStore />
+      <Footer />
+
+      {/* Video Player */}
+      <PopupPlayer isPlay={isPlay} togglePlay={togglePlay} />
+    </main>
+  );
+};
+
+export default App;
--- src/usuarios_frontend/src/components/Home.jsx
+++ src/usuarios_frontend/src/components/Home.jsx
@@ -2,7 +2,7 @@ const Home = () => {
   
   return (
     <section className="mt-5 text-center">
-      <h1>Bienvenido al mejor equipo de todos</h1>
+      <h1>this is a home landing page</h1>
     </section>
   )
 }
